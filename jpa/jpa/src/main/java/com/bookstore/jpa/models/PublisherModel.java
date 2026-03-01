f9b1f11bd05a19307f61af007eb3e46219020e71
João Paulo <136180000+jpaulolacerdadev@users.noreply.github.com>
2026-02-28 23:22:35 -0300

    Add files

---
+++ jpa/jpa/src/main/java/com/bookstore/jpa/models/PublisherModel.java
@@ -0,0 +1,35 @@
+package com.bookstore.jpa.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_PUBLISHER")
+public class PublisherModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
